146507338baf4d8d66ad047aea98624e81040ff0
Charles Lindsay <chaz@chazomatic.us>
2013-02-12 22:35:14 -0800

    License LGPL

--- StrobeTuner.h
+++ StrobeTuner.h
@@ -1,3 +1,21 @@
+/*
+  StrobeTuner - Arduino library for simple strobe tuners
+  Copyright 2013 Charles Lindsay
+
+  StrobeTuner is free software: you can redistribute it and/or modify it under
+  the terms of the GNU Lesser General Public License as published by the Free
+  Software Foundation, either version 3 of the License, or (at your option) any
+  later version.
+
+  StrobeTuner is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+  details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with StrobeTuner.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef STROBETUNER_H
 #define STROBETUNER_H
 
