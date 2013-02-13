206b648b321832c3d92660ffdf88c11883c8c0a8
Charles Lindsay <chaz@chazomatic.us>
2013-02-12 21:58:35 -0800

    Fix nits

--- StrobeTuner.h
+++ StrobeTuner.h
@@ -4,13 +4,13 @@
 /*
 Example circuit:
 ___
- A |-(out pin 1) ---|>|-----+
- R |               LED1(R)  |
- D |                        |
- U |-(out pin 2)----|>|-----+
- I |               LED2(G)  |
- N |             ______     |
- O |-(GND) -----|______|----+
+ A |-(LED1 pin) ---|>|-----+
+ R |              LED1(R)  |
+ D |                       |
+ U |-(LED2 pin)----|>|-----+
+ I |              LED2(G)  |
+ N |             ______    |
+ O |-(GND) -----|______|---+
 ___|             R1(160)
 
 */
@@ -36,8 +36,8 @@ class StrobeTuner {
     // Set the tuning note from its period in microseconds.  Call once to set
     // the note; update() actually flashes the LEDs.  note_period is the period
     // (1/frequency) of the note to tune to, in microseconds (so actually,
-    // 1000000/frequency in Hz).  current_micros is the current value of
-    // micros() typecast to unsigned int.
+    // 1000000/frequency in Hz).  Pass 0 to turn off the tuner.  current_micros
+    // is the current value of micros() typecast to unsigned int.
     void tune(unsigned int note_period, unsigned int current_micros);
 
     // Run the state machine, flashing the LEDs on the pins given in the
